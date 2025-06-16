e5fe566a715c1391e8356a6a0c5d4bf30886193e
Naimatul Ulumiyah <naemaaa89@gmail.com>
2025-06-16 23:05:52 +0700

    update terbaru

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -21,35 +21,41 @@
                 <h3 class="text-xl font-semibold mb-4 font-lato">Ikuti Kami</h3>
                 <ul class="space-y-2">
                     <li class="flex items-center space-x-2">
-                    <div class="w-10 h-10 bg-white rounded-full flex items-center justify-center overflow-hidden">
-                        <img src="{{ asset('images/footer/instagram.jpg') }}" alt="Instagram" class="w-10 h-10 object-contain">
+                    <div class="w-10 h-10 bg-white-hidden rounded-full flex items-center justify-center overflow-hidden">
+                        <img src="{{ asset('images/footer/instagramm.svg') }}" alt="Instagram" class="w-full h-full object-contain">
                      </div>
                 <a href="#" class="hover:underline">Instagram</a>
                 </li>
 
                     <li class="flex items-center space-x-2">
-                        <div class="w-10 h-10 bg-white rounded-full flex items-center justify-center">
-                            <span class="text-black">WA</span>
+                        <div class="w-10 h-10 bg-white-hidden rounded-full flex items-center justify-center">
+                        <img src="{{ asset('images/footer/WhatsApp.webp') }}" alt="WhatsApp" class="w-full h-full object-contain">
                         </div>
                         <a href="#" class="hover:underline">Whatsapp</a>
                     </li>
                     <li class="flex items-center space-x-2">
-                        <div class="w-10 h-10 bg-white rounded-full flex items-center justify-center">
-                            <span class="text-black">FB</span>
+                        <div class="w-10 h-10 bg-white-hidden rounded-full flex items-center justify-center">
+                        <img src="{{ asset('images/footer/facebook.png') }}" alt="Facebook" class="w-full h-full object-contain">
                         </div>
                         <a href="#" class="hover:underline">Facebook</a>
                     </li>
                 </ul>
             </div>
             <div>
-                <h3 class="text-xl font-semibold mb-4 font-lato">Metode Pembayaran</h3>
-                <div class="flex space-x-2">
-                    <div class="w-12 h-8 bg-purple-900 rounded"></div>
-                    <div class="w-12 h-8 bg-sky-700 rounded"></div>
-                    <div class="w-12 h-8 bg-sky-500 rounded"></div>
-                    <div class="w-12 h-8 bg-white border border-gray-300 rounded"></div>
-                </div>
-            </div>
+                <h3 class="text-xl font-semibold mb-7 font-lato">Metode Pembayaran</h3>
+                <div class="flex space-x-5">
+                    <div class="w-12 h-8 bg-purple-900 rounded">
+                        <img src="{{ asset('images/footer/ovo.jpg') }}" alt="ovo" class="w-full h-full object-contain">
+                    </div>
+                    <div class="w-12 h-8 bg-purple-900 rounded">
+                        <img src="{{ asset('images/footer/ovo.jpg') }}" alt="ovo" class="w-full h-full object-contain">
+                    </div>
+                    <div class="w-12 h-8 bg-purple-900 rounded">
+                        <img src="{{ asset('images/footer/ovo.jpg') }}" alt="ovo" class="w-full h-full object-contain">
+                    </div>
+                    <div class="w-12 h-8 bg-purple-900 rounded">
+                        <img src="{{ asset('images/footer/ovo.jpg') }}" alt="ovo" class="w-full h-full object-contain">
+                    </div>
         </div>
     </footer>
 </body>
